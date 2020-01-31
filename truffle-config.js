d67173b1ec4b08e566e88dd3b08f3f0730215235
Brandon Lucas <thebrandonlucas@gmail.com>
2020-01-31 09:28:29 -0600

    modifications made for ropsten deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,10 @@
 require('babel-register');
 require('babel-polyfill');
 
-const MNEMONIC = '0x4cB7a390728d9B8407DD9E7e4b87ad4040Fe92dE'
+
+var HDWalletProvider = require("truffle-hdwallet-provider");
+const MNEMONIC = 'bonus praise flower copy ahead wreck dog all erosion absent donor circle'
+const API_KEY = '6eec03fc82e0451b8ce49ef9da96453a'
 
 module.exports = {
   networks: {
@@ -13,7 +16,7 @@ module.exports = {
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/6eec03fc82e0451b8ce49ef9da96453a")
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/"+API_KEY)
       },
       network_id: 3,
       gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
