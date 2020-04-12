684a92e30f24bea6b0e8b9e21e0747d450879209
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2020-04-12 18:35:19 -0400

    Updates

---
+++ precache-manifest.88664f7efd98034870f0910ab1250030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51ec9d1e934aa360e98b",
+    "url": "/combat-deepfakes/static/js/runtime~main.51ec9d1e.js"
+  },
+  {
+    "revision": "1799b756dc47577ce74a",
+    "url": "/combat-deepfakes/static/js/main.1799b756.chunk.js"
+  },
+  {
+    "revision": "555ee3ff056b604b5eae",
+    "url": "/combat-deepfakes/static/js/1.555ee3ff.chunk.js"
+  },
+  {
+    "revision": "1799b756dc47577ce74a",
+    "url": "/combat-deepfakes/static/css/main.8afb4968.chunk.css"
+  },
+  {
+    "revision": "555ee3ff056b604b5eae",
+    "url": "/combat-deepfakes/static/css/1.08fc9a7d.chunk.css"
+  },
+  {
+    "revision": "1023c16945e25872805357ab78a43ff2",
+    "url": "/combat-deepfakes/index.html"
+  }
+];
